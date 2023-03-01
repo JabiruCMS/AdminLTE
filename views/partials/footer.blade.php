8b39fe463b9b75a5477c82356c4136e158a74900
David Cap <david.cap@bocians.net>
2023-02-28 22:06:13 -0700

    remove AsgardCMS copyright

--- views/partials/footer.blade.php
+++ views/partials/footer.blade.php
@@ -19,6 +19,6 @@
     </div>
 
     @yield('footer')
-    <strong>Copyright © {{ date('Y')}} <a href="http://asgardcms.com" target="_blank">AsgardCms</a></strong>
+    <strong>Copyright © {{ date('Y')}} <a href="https://bocian.cz" target="_blank">JabiruCMS</a></strong>
 </footer>
 
